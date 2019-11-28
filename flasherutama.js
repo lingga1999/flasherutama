52fefae5fda2b0b0ed999cd286a4c3d1255c4083
lingga1999 <47407292+lingga1999@users.noreply.github.com>
2019-11-28 09:41:46 +0700

    Update flasherutama.js

--- flasherutama.js
+++ flasherutama.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="4-ways-how-you-can-quickly-improve-your.html"}
 if(x=="3"){xxx="the-importance-of-hiring-property.html"}
 if(x=="4"){xxx="things-one-should-know-before-buying.html"}
 if(x=="5"){xxx="how-credit-card-consolidation-companies.html"}
-if(x=="6"){xxx="things-to-consider-before-buying-car.html"}a_to_ve[i].href="https://berkas.sobatflasher.com/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="things-to-consider-before-buying-car.html"}a_to_ve[i].href="https://unduh21.blogspot.com/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
